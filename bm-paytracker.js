b205e3adab710436236e7b81ee7c34663f638db1
Monty Dimkpa <cmdimkpa@gmail.com>
2018-06-30 13:50:12 +0100

    ver 0.5

--- bm-paytracker.js
+++ bm-paytracker.js
@@ -1,27 +1,84 @@
-// Javascript Library for the BlockMagic Payment Tracker application
-// ver 0.5
+/* Javascript Library for the BlockMagic Payment Tracker application
+(c) Monty Dimkpa, 2018
+ver: 0.5
+*/ 
 
-var httpdata;
+var httpdata;  // global message variable
+
+var payment_tracks = new Object();
+
+payment_tracks["utility"] = new Object();
+// add other payment tracks as required
+
+payment_tracks["utility"]["api_key"] = `47d7c7aec7471f59626b1423915bd3d8`;
+// set valid API keys for other tracks as required
+
+payment_tracks["utility"]["global_payee"] = `Acme Systems Limited`;
+// set a global payee for other tracks as required
+
+
 var base_urls = new Object();
 
-base_urls["new_account"] = `http://api.click-meter.com:5000/payment-tracker/create-account`;
 base_urls["push_receipt"] = `http://api.click-meter.com:5000/payment-tracker/push-receipt/`;
 base_urls["payment_summary"] = `http://api.click-meter.com:5000/payment-tracker/payment-summary/`;
 
 
-// for Node test only
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;   
+
+function process_push(push_url){
+		const xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", () => {});
+		xhr.open("GET", push_url);
+		xhr.send();
+}
 
 
-const xhr = new XMLHttpRequest();
+function process_query(query_url){
+		const xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", () => {
+			httpdata=JSON.parse(xhr.responseText);
+		});
+		xhr.open("GET", query_url);
+		xhr.send();
+}
 
-xhr.addEventListener("load", () => {
-    const data = JSON.parse(xhr.responseText);
-    console.log(data);
-	});
 
-xhr.open("GET", base_urls.new_account);
-xhr.send();
+function push_receipt(from,amount,track){
+
+	var data = new Object();
+
+	data["from"] = from.toLowerCase();
+	data["to"] = payment_tracks[track].global_payee.toLowerCase();
+	data["amount"] = amount;
+	data["key"] = payment_tracks[track].api_key;
+
+	var suffix = JSON.stringify(data);
+	var push_url = base_urls.push_receipt+suffix;
+
+	process_push(push_url);  // no need to evaluate, will succeed
+
+}
+
+
+function query_payments(from,track,from_date,to_date){
+
+	var data = new Object();
+
+	data["payer"] = from.toLowerCase();
+	data["payee"] = payment_tracks[track].global_payee.toLowerCase();
+	data["from_date"] = from_date;
+	data["to_date"] = to_date;
+	data["key"] = payment_tracks[track].api_key;
+
+	var suffix = JSON.stringify(data);
+	var query_url = base_urls.payment_summary+suffix;
+	
+	process_query(query_url);  // will refresh httpdata, wait some then evaluate
+}
+
+
+
+
+
 
 
 
