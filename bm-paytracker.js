b5e6a0693c836f5dddad7062456230bece4415fc
Monty DImkpa <cmdimkpa@gmail.com>
2018-07-01 17:31:26 +0100

    v0.5 final

--- bm-paytracker.js
+++ bm-paytracker.js
@@ -3,49 +3,24 @@
 ver: 0.5
 */ 
 
-var httpdata;  // global message variable
-
 var payment_tracks = new Object();
 
 payment_tracks["utility"] = new Object();
-// add other payment tracks as required
+// modify and add other payment tracks as required
 
-payment_tracks["utility"]["api_key"] = `47d7c7aec7471f59626b1423915bd3d8`;
-// set valid API keys for other tracks as required
+payment_tracks["utility"]["api_key"] = `7f2f09dafb3fa9f7c3c22377ad76203c`;
+// modify and set valid API keys for other tracks as required
 
 payment_tracks["utility"]["global_payee"] = `Acme Systems Limited`;
-// set a global payee for other tracks as required
-
+// modify and set a global payee for other tracks as required
 
 var base_urls = new Object();
 
 base_urls["push_receipt"] = `http://api.click-meter.com:5000/payment-tracker/push-receipt/`;
 base_urls["payment_summary"] = `http://api.click-meter.com:5000/payment-tracker/payment-summary/`;
 
-
-
-function process_push(push_url){
-		const xhr = new XMLHttpRequest();
-		xhr.addEventListener("load", () => {});
-		xhr.open("GET", push_url);
-		xhr.send();
-}
-
-
-function process_query(query_url){
-		const xhr = new XMLHttpRequest();
-		xhr.addEventListener("load", () => {
-			httpdata=JSON.parse(xhr.responseText);
-		});
-		xhr.open("GET", query_url);
-		xhr.send();
-}
-
-
 function push_receipt(from,amount,track){
-
 	var data = new Object();
-
 	data["from"] = from.toLowerCase();
 	data["to"] = payment_tracks[track].global_payee.toLowerCase();
 	data["amount"] = amount;
@@ -53,40 +28,40 @@ function push_receipt(from,amount,track){
 
 	var suffix = JSON.stringify(data);
 	var push_url = base_urls.push_receipt+suffix;
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", push_url, false );
+    xmlHttp.send( null );
+    alert(xmlHttp.responseText);
+ }
 
-	process_push(push_url);  // no need to evaluate, will succeed
-
-}
-
-
-function query_payments(from,track,from_date,to_date){
-
+function query_payments(track,from_date,to_date){
 	var data = new Object();
 
-	data["payer"] = from.toLowerCase();
-	data["payee"] = payment_tracks[track].global_payee.toLowerCase();
+	data["payer"] = `*`;
+	data["payee"] = `*`;
 	data["from_date"] = from_date;
 	data["to_date"] = to_date;
 	data["key"] = payment_tracks[track].api_key;
 
 	var suffix = JSON.stringify(data);
 	var query_url = base_urls.payment_summary+suffix;
-	
-	process_query(query_url);  // will refresh httpdata, wait some then evaluate
-}
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", query_url, false );
+    xmlHttp.send( null );
+    // get JSON data
+    var data = JSON.parse(xmlHttp.responseText);
+    // parse html
+    var total = data.total;
+    var events = data.events;
+    var html = `<tr><th>Date</th><th>From</th><th>To</th><th>Amount</th><th>Total</th></tr>`;
+    events.forEach((event)=>{
+    	if(events.indexOf(event)==0){
+    		html+=`<tr><td>`+event.date+`</td><td>`+event.from+`</td><td>`+event.to+`</td><td>`+event.amount+`</td><td>`+total.toString()+`</td></tr>`;
+    	}else{
+    		html+=`<tr><td>`+event.date+`</td><td>`+event.from+`</td><td>`+event.to+`</td><td>`+event.amount+`</td><td></td></tr>`;
+    	}
+    });
+    // embed in div
+    $("#bm-paytracker-table").html(html)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ }
